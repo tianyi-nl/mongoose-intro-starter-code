793075fe044b8eabf17d063d0b2a6c39c879821e
Tianyi Xue <tianyixue@MacBookPro.home>
2026-09-02 23:36:29 +0200

    code along with notes

---
+++ models/artist.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+// creating the schema ==> gives format
+const artistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  awardsWon: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+  isTouring: Boolean,
+  genre: {
+    type: String,
+    enum: ["rock", "alternative","pop","punk","nu-metal"],
+  },
+});
+
+// creating the Model ==> tool allow use to go into the collection to create modify etc..
+
+const Artist = mongoose.model("Artist", artistSchema);
+// internal name of the model. Always signular, capitlized and signle word.
+
+module.exports = Artist; // export things in ES5 or comonJS
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-process.loadEnvFile()
+try {
+  process.loadEnvFile()
+} catch (error) {
+  console.log("no .env found, using default variables if any")
+}
 
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/artist-db")
+.then(() =>{
+console.log("connected to the database yeah!")
+})
+.catch(()=>{
+  console.log("error connetecting to the database")
+})
 
 const app = express();
 
@@ -25,9 +38,99 @@ app.get("/", (req, res, next) => {
   res.json({ message: "all good here!" })
 })
 
+app.get("/test/:userId", (req, res, next) => {
+
+console.log("req.body", req.body)
+console.log("req.params", req.params)
+console.log("req.query", req.query)
+
+res.json({ message: "all good here from / test!" })
+})
+
+//* routes fro Artists
+const Artist = require("./models/artist.model");
+
+app.post("/artists", (req, res, next) => {
+  console.log(req.body);
+
+  Artist.create({
+    name: req.body.name,
+    awardsWon: req.body.awardsWon,
+    isTouring: req.body.isTouring,
+    genre: req.body.genre
+  })
+    .then(() => {
+      console.log("artist created");
+      res.send("artist created all good");
+    })
+    .catch((error) => {
+      console.log("error", error);
+      res.status(500).json({
+        message: "Error creating artist",
+        error: error.message
+      });
+    });
+});
+
+app.get("/artists",(req, res, next)=> {
+
+
+
+  Artist.find(req.query) //  for small filter, can be also :Artist.find({})
+.select({name: 1, awardsWon: 1}) // only sending these two properties to client 
+.sort({name: 1})                  // same as mongoCompass
+
+.then((response)=>{
+  res.json(response)
+
+})
+.catch((error)=>{
+res.json(error)
+})
+
+})
+
+//get one by ID
+app.get("/artists/:artistId", async(req, res, next) => {
+try {
+  const response = await Artist.findById(req.params.artistId)
+  res.json(response)
+
+} catch (error) {
+  res.json(error)
+}
+
+})
+
+
+//edit
+
+app.put("/artists/:artistId", async(req, res, next) =>{
+console.log(req.params)
+console.log(req.body)
+
+
+try {
+  const response = await Artist.findByIdAndUpdate(req.params.artistId, {
+    name: req.body.name,
+    awardsWon: req.body.awardsWon,
+    isTouring: req.body.isTouring,
+    genre: req.body.genre
+  },{ returnDocument: "after", // give the document after the update was applied
+      runValidators:  true // check schema validators before making modifiations
+    
+  })
+
+  res.json(response)
+  
+} catch (error) {
+  res.json(error)
+}
+
+})
 
 // server listen & PORT
-const PORT = process.env.PORT || 5005
+const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
